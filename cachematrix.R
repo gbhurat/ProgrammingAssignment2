a1a980641b92bfab4fdc070d2431e03f7bf79061
gbhurat <63623851+gbhurat@users.noreply.github.com>
2020-05-25 23:31:21 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,22 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        i <- NULL
-        set <- function(y){
-                m <<- y
-                i <<- NULL
+       ## define the argument with default mode of "matrix"
+        i <- NULL ## initialize i as NULL; will hold value of matrix inverse 
+        set <- function(y){ ## define the set function to assign new
+                m <<- y  ## value of matrix in parent environment
+                i <<- NULL ## if there is a new matrix, reset inv to NULL
         }
-        get <- function(){
+        get <- function(){ ## define the get fucntion - returns value of the matrix argument
                 m
         }        
         
-        setInverse <- function(inverse){
-                i <<- inverse
+        setInverse <- function(inverse){ 
+                i <<- inverse ## assigns value of i in parent environment
         } 
         getInverse <- function(){
-                i
+                i ## gets the value of i where called
         }
-        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) ##you need this in order to refer 
+                                                                                     ## to the functions with the $ operator
+
 }
 
 
@@ -28,12 +31,19 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getInverse()
+        
+        ## if the inverse has already been calculated
+        
         if(!is.null(m)){
+                ## get it from the cache and skips the computation
                 message("getting cached data")
                 return(m)
         }
+        ## otherwise, calculates the inverse 
         data <- x$get()
         m <- solve(data, ...)
+        
+        ## sets the value of the inverse in the cache via the setinv function
         x$setInverse(m)
         m
 }
